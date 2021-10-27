b0d9420491f2587bb658d5c46bb0a7d2a08f615d
Daniel <dsouzaborini@gmail.com>
2021-10-27 16:45:12 -0300

    add data handling in response

--- src/services/reposService.js
+++ src/services/reposService.js
@@ -1,12 +1,11 @@
-const Joi = require('joi');
-// const { parse, stringify } = require('flatted');
+// const Joi = require('joi');
 const reposModel = require('../models/reposModel');
 
 const genericError = {
   err: {
     status: 500,
     message: {
-      message: 'Opa, alguma coisa deu errado :(',
+      message: 'Erro na requisição ao GitHub',
     },
 } };
 
@@ -21,30 +20,38 @@ const langNotFoundError = {
 
 const getEssentialInfo = (repos) => {
   const filter = repos.map(({ name, description, language, owner: { avatar_url } }) => {
-    console.log('nao lint');
-    return { name, description, language, avatar_url };
+    const info = { name, description, language, avatar_url };
+    return info;
   });
   return filter;
+};
+
+// Source: https://forum.blip.ai/t/acessando-dados-de-um-array-de-objetos/11860/11s
+const arrayToDictionary = (array) => {
+  const dictionary = {};
+  array.forEach((value, index) => {
+      dictionary[index] = value;
+  });
+  return dictionary;
 };
 
 const filterByLang = (repos, lang) => {
   const essentialRepos = getEssentialInfo(repos);
   const filter = essentialRepos.filter(({ language }) => language === lang);
   if (filter.length < 1) return langNotFoundError;
-  return {
-    resp: {
-      status: 200,
-      content: filter,
-    },
-  };
+  return arrayToDictionary(filter);
 };
 
 const getRepos = async (info) => {
   const { lang } = info;
-  console.log(lang);
+  const repos = await reposModel.getRepos(info);
   try {
-    const repos = await reposModel.getRepos(info);
-    return filterByLang(repos.data, lang);
+    return {
+      resp: {
+        status: 200,
+        content: filterByLang(repos.data, lang),
+      },
+    };
   } catch (e) { return genericError; }
 };
 
